ac7f3dbd2ec7284026e706ff76b81b01ce258639
Aman Singh <amanpiyush2708@gmail.com>
2025-07-11 00:14:32 +0530

    data and Comparison Updated

---
+++ camparison.js
@@ -0,0 +1,22 @@
+console.log("2" > 1); //JS automatically convert in Number -- We should compare to same Datatype only.
+
+//Typescript give a strict rule 
+
+console.log(null);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log("2" > "Hello"); // false
+
+
+
+//camparion and equality check are different
+
+//Stirct Check
+
+console.log("2" === 2);
+console.log("2" === "2");
+
+
+
+//Write clean Code
--- datatype.js
+++ datatype.js
@@ -16,6 +16,45 @@ console.log(state);
 //object
 
 console.log(typeof stat);
-
+ 
 //NUll is an object
 //undefined is an undefined
+
+
+
+/***************** DataType ***************/
+//js is dynamic typed Languaged
+
+//Primitive
+// string | Boolean | Number | BigInt
+
+//Refernce (Non - primitive)
+//Array | Object | Functions
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+console.log(id === anotherId);
+
+
+
+let Fruits = ['Apple', 'Banana', 'Grapes', 'Mango']
+console.log(Fruits);
+console.log(typeof (Fruits));
+
+let user = {
+    Name : 'Tanjiro',
+    Age : 19,
+    Work : 'Demon slayer'
+}
+
+let Number = function Hello (){
+    for(let i = 0;i<=10;i++){
+        console.log("Demon Slayer");
+        
+    }
+}
+Number();
+
+console.log(typeof(Number));
+console.log(typeof(id));
