34339e242cfd80cc17dba9eb111ab63e3d49ee8f
Aman Singh <amanpiyush2708@gmail.com>
2025-07-10 12:17:07 +0530

    Conversion and Datatype

---
+++ conversion.js
@@ -0,0 +1,14 @@
+let score = "100"; // String
+console.log(`My score is ${score} out of 100`);
+
+let percentage = Number(score);
+console.log(typeof (percentage)); // Number
+// console.log(percentage); // NaN = Not a Number 
+
+
+let someNumber = 34;
+let Numberconvert = String(someNumber);
+console.log(Numberconvert);
+console.log(typeof (Numberconvert));
+
+
---
+++ datatype.js
@@ -0,0 +1,21 @@
+let Name = "Tanjiro Kamado";
+console.log("Name: " + Name);
+let stat = null;
+console.log(stat);
+let state;
+console.log(state);
+
+// Data Types
+//Number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol  == Unique
+//object
+
+console.log(typeof stat);
+
+//NUll is an object
+//undefined is an undefined
