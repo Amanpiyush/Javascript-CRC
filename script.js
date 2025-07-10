071370f2833f173f7cff7ff8cd26b8ed37b09a5e
Aman Singh <amanpiyush2708@gmail.com>
2025-07-10 07:25:07 +0530

    Basic Let, const Var

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("DEMON SLAYER");
+console.log("TANJIORO");
+let tanjiro = "TANJIRO";
+const Gojo = "GOJO";
+var sakura = "SAKURA";
+const demonSlayer = {
+    First : "Tanjiro",
+    Second : "Nezuko",
+    Third : "Zenistu",
+    Fourth : "Inosuke",
+    Fifth : "Rengoku",
+}
+
+let Windbreaker;
+console.log(demonSlayer.Fourth);
+console.table([Gojo, tanjiro, sakura,demonSlayer, Windbreaker]);
+
+//var is not use in the modern days,, pefer to use let, const
