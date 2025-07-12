96e57872e6576e2e678a56fecf85e7ce3fe385ed
Aman Singh <amanpiyush2708@gmail.com>
2025-07-12 23:31:53 +0530

    IIFE

---
+++ IIFE.js
@@ -0,0 +1,10 @@
+//Immediately Invoked Function Expression
+(function chai (){
+    //Named IIFE 
+    console.log("Hello Buddy");
+    
+})();
+
+((Name)=>{
+    console.log(`My name is ${Name}`);
+})('Hitesh')
