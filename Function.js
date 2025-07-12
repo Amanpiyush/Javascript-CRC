9fa1a30c41fb71ef9e8445078d7f92631565e5bd
Aman Singh <amanpiyush2708@gmail.com>
2025-07-13 03:26:57 +0530

    Callstack and Function

--- Function.js
+++ Function.js
@@ -1,55 +1,68 @@
-//Functions
-let Number = (num1 , num2) =>{
-    return num1+num2;
-}
+// //Functions
+// let Number = (num1 , num2) =>{
+//     return num1+num2;
+// }
 
-const Result = Number(4, 6);
-// console.log(Result);
+// const Result = Number(4, 6);
+// // console.log(Result);
 
 
 
-// let Message = (username)=>{
-//     if(username === undefined){
-//         console.log("Please Enter the USername");
+// // let Message = (username)=>{
+// //     if(username === undefined){
+// //         console.log("Please Enter the USername");
+// //         return;
+// //     }
+// //     return `${username} just logged in`;
+// // }
+
+// // const result = Message('Hiro'); //Undefined if we don't pass the value
+// // console.log(result);
+
+
+// //Professional Way to write the code
+// let Message = (username = 'sam')=>{
+//     if(!username){
+//         console.log("Please Enter the Username");
 //         return;
 //     }
 //     return `${username} just logged in`;
 // }
 
-// const result = Message('Hiro'); //Undefined if we don't pass the value
-// console.log(result);
+// const result = Message(); //Undefined if we don't pass the value
+// // console.log(result);
 
 
-//Professional Way to write the code
-let Message = (username = 'sam')=>{
-    if(!username){
-        console.log("Please Enter the Username");
-        return;
-    }
-    return `${username} just logged in`;
+
+// //Shopping cart : create a parameter that user dont know hoe many it can add the 
+
+// function calculatecartprice (...num1){ // Add the Multiple Items
+//     return num1
+// }
+
+// console.log(calculatecartprice(200, 400, 600, 900 ));
+
+
+// //==========================Array=======================//
+
+// const myNewArray = [ 200, 400, 600, 800];
+// function returnthearray (myarray){
+//     return myarray[1];
+// }
+
+// console.log(returnthearray(myNewArray));
+
+
+
+
+function one() {
+console.log('one')
+};
+
+function two(){
+    console.log('Two')
 }
 
-const result = Message(); //Undefined if we don't pass the value
-// console.log(result);
-
-
-
-//Shopping cart : create a parameter that user dont know hoe many it can add the 
-
-function calculatecartprice (...num1){ // Add the Multiple Items
-    return num1
+function three() {
+    console.log('Three')
 }
-
-console.log(calculatecartprice(200, 400, 600, 900 ));
-
-
-//==========================Array=======================//
-
-const myNewArray = [ 200, 400, 600, 800];
-function returnthearray (myarray){
-    return myarray[1];
-}
-
-console.log(returnthearray(myNewArray));
-
-
