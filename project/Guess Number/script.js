a80f3c4c074e3762811ad5ec292b5b0a828a0cfe
Aman Singh <amanpiyush2708@gmail.com>
2025-07-23 09:30:22 +0530

    Guess Number

--- project/Guess Number/script.js
+++ project/Guess Number/script.js
@@ -1,30 +1,52 @@
-const RandomNumber = parseInt(Math.random() * 100 + 1);
+const RandomNumber = Math.floor(Math.random() * 100 + 1);
+const Submit = document.querySelector('#subt');
+const GuessField = document.querySelector('.guessField').value;
 
-const submit = document.querySelector('#subt')
-const userInput = document.querySelector('#guessField').value;
-const Guess = document.querySelector('.guesses');
+const GuessSlot = document.querySelector('.guesses');
 const Remaining = document.querySelector('.lastResult');
+
 const LoworHigh = document.querySelector('.lowOrHi');
-const Startover = document.querySelector('.resultParas');
+const StartOver = document.querySelector('.resultParas');
 
 
-const p = document.createElement('p'); 
+const p = document.createElement('p');
 
+let PreviousGuess = [];
+let RemainGuess = 1
 
-let preGUess = [];
-let NumGUess = 1;
 let playGame = true;
 
-const Validate = ((ValidateGuess) => {
-    if(userInput < 0 || isNaN(userInput)){
-        submit.innerHTML = `You are Choosen Number is ${userInput} `
-        if(userInput > 100){
-            submit.innerHTML = 'You have select Number from 1-100'
-        }
-    }
+if(playGame){
+  Submit.addEventListener('click', (event) => {
+    event.previousDefault();
+    const guess = parseInt(userInput.value);
+    validation(guess)
+  })
+}
+
+const validation = ((Guess) => {
+  //Validate Guess
+  if (Guess < 0 || Guess > 100 || isNaN(Guess)){
+    alert('please a Valid Number')
+  }
 })
 
-const GuuessNumber = ((guess) => {
+const CheckGuess = ((Guess) => {
+  //Value rnadom == userInput
+})
+
+const DisplayGuess = ((Guess) => {
+  //display you won
+})
+
+const DisplayMessage = ((Message) => {
+  //Display Message
+})
+
+const endGame = ((Game) => {
 
 })
 
+const newGame = ((Game) => {
+
+})
